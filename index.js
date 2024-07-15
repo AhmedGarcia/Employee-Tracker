778927fbf7690e3c27b38a0a067fd6067c89adae
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-07-15 08:46:57 -0400

    started the main menu on index.js file. will implement the switch/case method to handle the choices inputed by user

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ const {
 } = require('./db/queries');
 
 
+const mainMenu = async () => {
+    const { action } = await inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit',
+
+        ],
+    });
+
+}

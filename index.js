24f5027e5377ef0c10b7f5047e3d1b27f03473ea
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-07-15 09:13:26 -0400

    added cases for all depts, all roles, all employees, add dept, and add role. will work on add employee and  update employee role

--- index.js
+++ index.js
@@ -29,4 +29,52 @@ const mainMenu = async () => {
         ],
     });
 
+    //Handles the user choice
+    switch (action) {
+        case 'View all departments':
+            const departments = await getAllDepartments(); //Calls function to get all depts
+            console.table(departments); // Displays all departments in a table
+            break;
+        case 'View all roles':
+            const roles = await getAllRoles(); // Calls function to get all roles
+            console.table(roles); //Displays all roles in table   
+            break;
+        case 'View all employees':
+            const employees = await getAllEmployees(); //Calls function to get all employees
+            console.table(employees); //Displays all employees in table
+            break; 
+        case 'Add a department':
+            const { departmentName } = await inquirer.prompt({
+                name: 'departmentName',
+                type: 'input',
+                message: 'Enter the name of the department:',
+            });
+            await addDepartment(departmentName);
+            console.log(`Added department: ${departmentName}`);
+            break;
+        case 'Add a role':
+            const departmentsList = await getAllDepartments();
+            const departmentChoices = departmentsList.map(({ id, name}) => ({ name, value: id })); 
+            const { roleTitle, roleSalary, roleDepartment } = await inquirer.prompt([
+                {
+                    name: 'roleTitle',
+                    type: 'input',
+                    meassage: 'Enter the title of the role:',
+                },
+                {
+                    name: 'roleSalary',
+                    type:'input',
+                    message: 'Enter the salary for the role:',
+                },
+                {
+                    name: 'roleDepartment',
+                    type: 'list',
+                    message: 'Select the department for the role:',
+                    choices: departmentChoices,
+                },
+            ]);
+            await addRole(roleTitle, roleSalary, roleDepartment);  
+            console.log(`Added role: ${roleTitle}`) ;
+            break;
+    }
 }
